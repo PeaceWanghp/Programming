7ba83cf9606dc9a9a16192e2984038c7e6954745
wangheping <wangheping@b.360.cn>
2020-07-08 19:46:17 +0800

    perfect categary

---
+++ Programming/Programming/Objective-C/Categary/UIView+Frame.h
@@ -0,0 +1,22 @@
+//
+//  UIView+Frame.h
+//  Objective-C
+//
+//  Created by peace on 2019/4/3.
+//  Copyright © 2019 peace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (Frame)
+
+- (CGFloat)x;
+- (CGFloat)y;
+- (CGFloat)width;
+- (CGFloat)height;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Programming/Programming/Objective-C/Categary/UIView+Frame.m
@@ -0,0 +1,29 @@
+//
+//  UIView+Frame.m
+//  Objective-C
+//
+//  Created by peace on 2019/4/3.
+//  Copyright © 2019 peace. All rights reserved.
+//
+
+#import "UIView+Frame.h"
+
+@implementation UIView (Frame)
+
+- (CGFloat)x {
+    return self.frame.origin.x;
+}
+
+- (CGFloat)y {
+    return self.frame.origin.y;
+}
+
+- (CGFloat)width {
+    return self.frame.size.width;
+}
+
+- (CGFloat)height {
+    return self.frame.size.height;
+}
+
+@end
